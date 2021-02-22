409ff16a9065aae10d083c5deb5ac109d194e71d
Charles Koh <koh.jc@kstc.co.kr>
2021-02-22 17:29:50 +0900

    first commit

---
+++ EFCountingLabel/EFCountingLabel.h
@@ -0,0 +1,18 @@
+//
+//  EFCountingLabel.h
+//  EFCountingLabel
+//
+//  Created by Charles Koh on 2021/02/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EFCountingLabel.
+FOUNDATION_EXPORT double EFCountingLabelVersionNumber;
+
+//! Project version string for EFCountingLabel.
+FOUNDATION_EXPORT const unsigned char EFCountingLabelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EFCountingLabel/PublicHeader.h>
+
+
---
+++ EFCountingLabel/README.md
@@ -0,0 +1,19 @@
+## EFCountingLabel  
+This project is framework that [EFCountingLabl][1] is working in Carthage build system.  
+
+### Cartage
+- in `Cartfile` file  
+  ```
+  ...
+  github "kstmobility/EFCountingLabel" ~> 5.1.2
+  ...
+  
+  ```
+- build  
+  ```
+  carthage bootstrap --platform iOS --cache-builds
+  ```
+
+
+[1]: https://github.com/EFPrefix/EFCountingLabel  
+
